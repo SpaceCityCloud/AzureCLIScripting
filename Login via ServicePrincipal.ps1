23abf4500507005efc29ef2ec5838fb4e9100201
Chris Frizzell <115434067+SpaceCityCloud@users.noreply.github.com>
2022-10-09 19:07:28 -0500

    Delete Login via ServicePrincipal.ps1

--- Login via ServicePrincipal.ps1
+++
@@ -1,4 +0,0 @@
-﻿$applicationId = "xyz";   #Operator-ID
-$securePassword = "abc" | ConvertTo-SecureString -AsPlainText -Force   #Operator-Key
-$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $applicationId, $securePassword
-Connect-AzureRmAccount -ServicePrincipal -Credential $credential -TenantId "a-b-c"     #TenantID
